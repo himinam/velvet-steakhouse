90817df4e353e43547122a8fbe5071a4525cfffa
FerPorres <mafesitariascos1213@gmail.com>
2026-05-19 14:47:57 -0500

    actualizacion restaurante

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Velvet Steakhouse 🚀");
+
+ScrollReveal().reveal('.hero-content',{
+    delay:300,
+    distance:'60px',
+    origin:'bottom',
+    duration:1200
+});
+
+ScrollReveal().reveal('.card',{
+    interval:200,
+    distance:'50px',
+    origin:'bottom'
+});
+
+ScrollReveal().reveal('.galeria-grid img',{
+    interval:200,
+    scale:.8
+});
+
+ScrollReveal().reveal('.testimonio',{
+    interval:200,
+    origin:'bottom'
+});
